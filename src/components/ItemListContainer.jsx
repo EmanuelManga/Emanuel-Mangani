d7cdc8520e7d2201ee7b6a9f20194cb96c262427
emanuel <emanuel_mangani@yahoo.com>
2022-06-05 14:40:37 -0300

    desafio 4 botones suma resta y agregar al carrito Emanuel Mangani

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
-export default function ItemListContainer({mostrarAlgo}) {
+
+export default function ItemListContainer() {
+
+    // const [productos, setProductos] = useState ([
+    //     {nombre: 'Latte Machiato',id: 1 , stock: 15},
+    //     {nombre: 'Latte Machiato Vanilla',id: 2 , stock: 10},
+    //     {nombre: 'Latte Machiato Caramel',id: 3 , stock: 20},
+    //     {nombre: 'Cafe Mocha',id: 4 , stock: 8}
+    // ]);
+
+    const onAdd = (count) => {
+        alert(`sumaste ${count} al carrito`)
+    }
+
     return (
-        <div>{mostrarAlgo}</div>
+        <div>
+            <ItemCount cantInicial={1} stock={15} onAdd={onAdd}> </ItemCount>
+        </div>
     )
 }
