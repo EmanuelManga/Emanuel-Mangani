46ed7334cf0d149c2d1c92cc5dad2d89bc8ba98d
emanuel <emanuel_mangani@yahoo.com>
2022-06-07 09:39:45 -0300

    desafio 5 map y promise - Comentarios con dudas  - Emanuel Mangani

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -5,9 +5,17 @@ import {Button,Card,Col} from "react-bootstrap";
 export default function ItemCount({item,cantInicial,onAdd}) {
 
     const [contItem,setContItem] = useState(cantInicial)
-    const [contCarrito,setContCarrito] = useState([]);
 
+
+    const [contCarrito,setContCarrito] = useState([]);
+    // cuando tenia un solo producto utilice este useState con el valor de Stock hardcoreado en el que le iba restando la cantidad 
+    // que agregaba al carrito para saber si esta dentro del stock disponible, el problema es que con varios producto este state deberia
+    // ser un array en el que cada uno de los valores corresponda a cada producto, no puedo decifrar como hacerlo,  no encontre una 
+    // solucion en internet, y de la varias formas que probe creo que lo mas logico seria con una funcion filter al array de productos 
+    // (pero no me salio). Y por lo que entendi segun lo que me mandaste al chat creo que este es el orden correcto de los componentes
     
+
+
     const sumar = () =>{
         if(contItem < contCarrito)
             setContItem(contItem+1);
@@ -58,7 +66,6 @@ export default function ItemCount({item,cantInicial,onAdd}) {
                     <Button variant="primary" onClick={agregarCarrito}>Agregar al carrito </Button>
                 </Card.Body>
             </Card> 
-
         </Col>
         </>
     )
